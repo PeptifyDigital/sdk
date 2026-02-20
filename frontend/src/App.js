fca77e72f36edbc78adc7decf7aef09cc40e5fa3
Peptify <muhammadtaufikgiga@gmail.com>
2026-02-20 20:10:15 +0700

    Create App.js

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import PeptideList from "./components/PeptideList";
+
+function App() {
+  return (
+    <div style={{ padding: "20px" }}>
+      <h1>Peptify Ecosystem</h1>
+      <PeptideList />
+    </div>
+  );
+}
+
+export default App;
